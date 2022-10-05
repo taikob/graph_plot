fd1f45b2e871776a46619851678699d8879fa280
taikob <taisukekbys@gmail.com>
2022-10-05 22:42:12 +0900

    renewal

--- graph_plot/data_processing.py
+++ graph_plot/data_processing.py
@@ -1,4 +1,6 @@
+import csv,sys,os,datetime,shutil
 import numpy as np
+from data_prcs import get as g
 
 def datafilter(data, fixparam):
 
@@ -88,23 +90,17 @@ def rangelimit(data, sysparam, nump, xnum, xmin, xmax,ax):
 
     return data, sysparam, nump
 
-def save_graph_data(title,data,sysparam,xnum,znum,lnum):
+def save_graph_data(dir,data,sysparam,xnum,ynum,znum,lnum):
     xdata=sysparam[xnum]
-    if znum is not None:
-        zdata=sysparam[znum]
-    if lnum is not None:
-        ldata=sysparam[lnum]
+    if znum is not None: zdata=sysparam[znum]
+    if lnum is not None: ldata=sysparam[lnum]
 
     tmpd=data.copy()#tmpdata
 
-    if znum==None and lnum!=None:
-        pdata=np.empty((tmpd.shape[0]* tmpd.shape[1]*tmpd.shape[2],3))
-    elif znum==None:
-        pdata=np.empty((tmpd.shape[0]* tmpd.shape[1]*tmpd.shape[2],1))
-    elif lnum==None:
-        pdata=np.empty((tmpd.shape[0]* tmpd.shape[1]*tmpd.shape[2],2))
-    else:
-        pdata=np.empty((tmpd.shape[0]* tmpd.shape[1]*tmpd.shape[2],3))
+    if znum==None and lnum!=None: pdata=np.empty((tmpd.shape[0]* tmpd.shape[1]*tmpd.shape[2],3))
+    elif znum==None: pdata=np.empty((tmpd.shape[0]* tmpd.shape[1]*tmpd.shape[2],1))
+    elif lnum==None: pdata=np.empty((tmpd.shape[0]* tmpd.shape[1]*tmpd.shape[2],2))
+    else: pdata=np.empty((tmpd.shape[0]* tmpd.shape[1]*tmpd.shape[2],3))
     sdata=np.empty((tmpd.shape[0]* tmpd.shape[1]*tmpd.shape[2],1))
 
     nz=0
@@ -112,14 +108,101 @@ def save_graph_data(title,data,sysparam,xnum,znum,lnum):
         for z in range(tmpd.shape[1]):
             sdata[tmpd.shape[0] * nz : tmpd.shape[0] * (nz + 1), 0]=tmpd[:,z,l]
             pdata[tmpd.shape[0] * nz : tmpd.shape[0] * (nz + 1), 0]=xdata
-            if znum is not None:
-                pdata[tmpd.shape[0] * nz : tmpd.shape[0] * (nz + 1), 1] = zdata[z]
-            if lnum is not None:
-                pdata[tmpd.shape[0] * nz : tmpd.shape[0] * (nz + 1), 2] = ldata[l]
-            if lnum is not None and znum is None:
-                pdata[tmpd.shape[0] * nz : tmpd.shape[0] * (nz + 1), 1] = 0
-
+            if znum is not None: pdata[tmpd.shape[0] * nz : tmpd.shape[0] * (nz + 1), 1] = zdata[z]
+            if lnum is not None: pdata[tmpd.shape[0] * nz : tmpd.shape[0] * (nz + 1), 2] = ldata[l]
+            if lnum is not None and znum is None: pdata[tmpd.shape[0] * nz : tmpd.shape[0] * (nz + 1), 1] = 0
             nz+=1
 
-    np.savetxt(title,np.hstack([pdata,sdata]), delimiter=",")
-    return np.hstack([pdata,sdata])
+    if not os.path.exists(dir): os.mkdir(dir)
+    dir += '/x' + str(xnum) + '_y' + str(ynum) + '_z' + str(znum) + '_l' + str(lnum) + '.csv'
+    np.savetxt(dir,np.hstack([pdata,sdata]), delimiter=",")
+    return dir
+
+def get_config(configpath,fixparaml=None):
+
+    if configpath is None or configpath is '':
+        print('Please set config file!')
+        import set_config as sc
+        sc.main(); sys.exit()
+    elif not os.path.exists('config/'+configpath+'.py'):
+        print('config file is not here! ', 'config/'+configpath+'.py')
+        sys.exit()
+
+    sys.path.append(os.getcwd()+'/config')
+    exec('import ' + configpath + ' as p')
+
+    global dataname; dataname = p.dataname
+    global fixparam; fixparam = fixparaml
+    global exceptnum;exceptnum=p.exceptnum
+    global exceptval;exceptval=p.exceptval
+    global exceptreq;exceptreq=p.exceptreq
+    global paramrow ;paramrow=p.paramrow
+    global xnum;     xnum=p.xnum
+    global ynum;     ynum=p.ynum
+    global znum;     znum=p.znum
+    global lnum;     lnum=p.lnum
+    global xmin;     xmin=p.xmin
+    global xmax;     xmax=p.xmax
+    global zmin;     zmin=p.zmin
+    global zmax;     zmax=p.zmax
+    global lmin;     lmin=p.lmin
+    global lmax;     lmax=p.lmax
+    global tc;       tc=p.tc
+    global cl;       cl=p.cl
+    global gl;       gl='-o'
+    global pc;       pc=p.pc
+    global addt;     addt=p.addt
+    global lineplot; lineplot=None
+    global xtitle;   xtitle=p.xtitle
+    global ytitle;   ytitle=p.ytitle
+    global hsize; hsize=1.89
+    global vsize; vsize=0.7
+    global xlog;   xlog=p.xlog
+    global ylog;   ylog=p.ylog
+    global nogrid; nogrid=p.nogrid
+    global xlm;    xlm =p.xlm
+    global xmal;   xmal=p.xmal
+    global xmil;   xmil=p.xmil
+    global ylm;    ylm =p.ylm
+    global ymal;   ymal=p.ymal
+    global ymil;   ymil=p.ymil
+    global xtick;   xtick=p.xtick
+    global ytick;   ytick=p.ytick
+    global mksize; mksize=3
+    global lnsize; lnsize=0.5
+    global dashline; dashline=None
+    global fitdata; fitdata=None
+
+    if hasattr(p,'lineplot'): lineplot=p.lineplot
+    if hasattr(p,'hsize' ): hsize=p.hsize
+    if hasattr(p,'vsize' ): vsize=p.vsize
+    if hasattr(p,'mksize'): mksize=p.mksize
+    if hasattr(p,'lnsize'): lnsize=p.lnsize
+    if hasattr(p, 'gl'): gl = p.gl
+    if fixparam == None: fixparam = p.fixparam
+
+def prepare_data(dataname,paramrow,xnum, ynum,lnum,fixparam,xmin=None,xmax=None,zmin=None,zmax=None,znum=None,lmin=None,lmax=None,exceptnum=None, exceptval=None, exceptreq=None):
+
+    with open(dataname) as f: data = [row for row in csv.reader(f)]
+
+    if exceptnum is not None: data=dataexcept(data, exceptnum, exceptval, exceptreq, ynum)
+    if fixparam != None: data=datafilter(data, fixparam)
+
+    sysparam, nump=g.get_sysparam(data,paramrow)
+    data=make_graphdata(data,sysparam,nump,xnum,ynum,znum,lnum)
+
+    if xmin is not None or xmax is not None: data, sysparam, nump = rangelimit(data, sysparam, nump, xnum, xmin, xmax,0)
+    if zmin is not None or zmax is not None: data, sysparam, nump = rangelimit(data, sysparam, nump, znum, zmin, zmax,1)
+    if lmin is not None or lmax is not None: data, sysparam, nump = rangelimit(data, sysparam, nump, lnum, lmin, lmax,2)
+
+    return data, sysparam, nump
+
+if __name__ == '__main__':
+
+    configpath='config_0'
+
+    get_config(configpath)
+    dir = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    data, sysparam, nump = prepare_data(dataname, paramrow, xnum, ynum, lnum, fixparam)
+    save_graph_data(dir, data, sysparam, xnum, znum, lnum)
+    shutil.copyfile('config/' + configpath + '.py', dir + '/config.py')
--- graph_plot/plot_data.py
+++ graph_plot/plot_data.py
@@ -1,105 +1,8 @@
-import sys, os, csv, argparse, shutil
-from data_prcs import get as g
-from color import convert as c
-import data_processing as d
+import sys, os, datetime
 import matplotlib.pyplot as plt
+import data_processing as d
+from data_prcs import get as g
 import numpy as np
-import datetime
-
-def get_config(configpath,dataname,rename,fixparam, xline, yline):
-
-    if configpath is None or configpath is '':
-        print('Please set config file!')
-        import set_config as sc
-        sc.main(); sys.exit()
-    elif not os.path.exists('config/'+configpath+'.py'):
-        print('config file is not here! ', 'config/'+configpath+'.py')
-        sys.exit()
-
-    sys.path.append(os.getcwd()+'/config')
-    exec('import ' + configpath + ' as p')
-
-    global exceptnum;exceptnum=p.exceptnum
-    global exceptval;exceptval=p.exceptval
-    global exceptreq;exceptreq=p.exceptreq
-    global paramrow ;paramrow=p.paramrow
-    global xnum;     xnum=p.xnum
-    global ynum;     ynum=p.ynum
-    global znum;     znum=p.znum
-    global lnum;     lnum=p.lnum
-    global xmin;     xmin=p.xmin
-    global xmax;     xmax=p.xmax
-    global zmin;     zmin=p.zmin
-    global zmax;     zmax=p.zmax
-    global lmin;     lmin=p.lmin
-    global lmax;     lmax=p.lmax
-    global ap;       ap=p.ap
-    global tc;       tc=p.tc
-    global cl;       cl=p.cl
-    global gl;       gl='-o'
-    global pc;       pc=p.pc
-    global addt;     addt=p.addt
-    global lineplot
-    global xtitle;   xtitle=p.xtitle
-    global ytitle;   ytitle=p.ytitle
-    global hsize; hsize=1.89
-    global vsize; vsize=0.7
-    global xlog;   xlog=p.xlog
-    global ylog;   ylog=p.ylog
-    global nogrid; nogrid=p.nogrid
-    global xlm;    xlm =p.xlm
-    global xmal;   xmal=p.xmal
-    global xmil;   xmil=p.xmil
-    global ylm;    ylm =p.ylm
-    global ymal;   ymal=p.ymal
-    global ymil;   ymil=p.ymil
-    global mksize; mksize=None
-    global lnsize; lnsize=None
-    global dashline; dashline=None
-    global fitdata; fitdata=None
-
-
-    if hasattr(p,'lineplot'): lineplot=p.lineplot
-    if hasattr(p,'hsize' ): hsize=p.hsize
-    if hasattr(p,'vsize' ): vsize=p.vsize
-    if hasattr(p,'mksize'): mksize=p.mksize
-    if hasattr(p,'lnsize'): lnsize=p.lnsize
-    if hasattr(p, 'gl'): gl = p.gl
-
-    if xline == None:
-        if hasattr(p, 'xline'): xline = p.xline
-    if yline == None:
-        if hasattr(p, 'yline'): yline = p.yline
-
-    if dataname == None: dataname = p.dataname
-    if fixparam == None: fixparam = p.fixparam
-
-    return dataname,rename,fixparam, xline, yline
-
-def prepare_data(dataname,fixparam):
-
-    with open(dataname) as f:
-        reader = csv.reader(f)
-        data = [row for row in reader]
-
-
-    if exceptnum is not None:
-        data=d.dataexcept(data, exceptnum, exceptval, exceptreq, ynum)
-
-    if fixparam != None:
-        data=d.datafilter(data, fixparam)
-
-    sysparam, nump=g.get_sysparam(data,paramrow)
-    data=d.make_graphdata(data,sysparam,nump,xnum,ynum,znum,lnum)
-
-    if xmin is not None or xmax is not None:
-        data, sysparam, nump = d.rangelimit(data, sysparam, nump, xnum, xmin, xmax,0)
-    if zmin is not None or zmax is not None:
-        data, sysparam, nump = d.rangelimit(data, sysparam, nump, znum, zmin, zmax,1)
-    if lmin is not None or lmax is not None:
-        data, sysparam, nump = d.rangelimit(data, sysparam, nump, lnum, lmin, lmax,2)
-
-    return data, sysparam, nump
 
 def plot_paraset():
 
@@ -113,8 +16,9 @@ def plot_paraset():
     plt.rcParams["ytick.direction"] = "in"
     plt.rcParams["xtick.top"] = True
     plt.rcParams["ytick.right"] = True
+    plt.rcParams["font.size"] = 10
 
-def graph_paraset(xline,yline,l):
+def graph_paraset():
 
     if xlog is not None: plt.xscale("log")
     if ylog is not None: plt.yscale("log")
@@ -125,145 +29,73 @@ def graph_paraset(xline,yline,l):
     if ylm  is not None: plt.ylim(ylm[0], ylm[1])
     if ymal is not None: plt.gca().yaxis.set_major_locator(plt.MultipleLocator(ymal))
     if ymil is not None: plt.gca().yaxis.set_minor_locator(plt.MultipleLocator(ymil))
-    if xline is not None: plt.hlines([xline[l]], xlm[0], xlm[1], "black", linewidth=0.7)  # hlines
-    if yline is not None: plt.vlines([yline[l]], ylm[0], ylm[1], "black", linewidth=0.7)  # hlines
-    if mksize is None: global mksize; mksize = 3
-    if lnsize is None: global lnsize; lnsize = 0.5
+    if xtick is not None: plt.xticks(color="None")
+    if ytick is not None: plt.yticks(color="None")
 
-def save(data, sysparam,configpath, rename, fig, lb, n):
-    tt = "x" + str(xnum) + "_y" + str(ynum) + "_z" + str(znum) + "_l" + str(lnum) + '_' + str(n)
-    if addt is not None: tt += addt
+def plot(data, configpath):
+    if configpath is None or configpath is '':
+        print('Please set config file!')
+        import set_config as sc
+        sc.main(); sys.exit()
+    elif not os.path.exists('config/'+configpath+'.py'):
+        print('config file is not here! ', 'config/'+configpath+'.py')
+        sys.exit()
 
-    d.save_graph_data(tt + '_graph.csv', data, sysparam, xnum, znum, lnum)
+    sys.path.append(os.getcwd()+'/config')
+    exec('import ' + configpath + ' as p')
 
-    fig.savefig(tt + '_output.pdf', transparent=True, bbox_inches='tight')
-    if lb is not None:
-        plt.legend().get_frame().set_alpha(1)
-        fig.savefig(tt + "_legend.pdf", transparent=True, bbox_inches='tight')
+    global tc, cl, gl, pc, addt,wspace,hspace, xline,yline,xtitle,ytitle, xtick, ytick, hsize, vsize,xlog,ylog,nogrid,xlm
+    global xmal,xmil,ylm,ymal,ymil,xplotnum,yplotnum,title,mksize,lnsize,dashline,fitdata
+    tc=p.tc; cl=p.cl; gl='-o'; pc=p.pc; addt=p.addt; wspace = p.wspace; hspace = p.hspace; vsize=0.7; xlog=p.xlog; ylog=p.ylog
+    xline=p.xline; yline=p.yline; xtitle=p.xtitle; ytitle=p.ytitle; xtick=p.xtick; ytick=p.ytick; hsize=1.89; nogrid=p.nogrid
+    xlm = p.xlm; xmal = p.xmal; xmil=p.xmil; ylm =p.ylm; ymal=p.ymal; ymil=p.ymil; xplotnum=p.xplotnum; yplotnum=p.yplotnum
+    title = p.title; mksize=3; lnsize=0.5; dashline=None; fitdata=None
 
-    if rename == None:
-        rename = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-    if not os.path.exists('graph'):
-        os.mkdir('graph')
-    os.rename(tt + '_graph.csv', 'graph/' + rename + '_' + tt + '_graph.csv')
-    os.rename(tt + '_output.pdf', 'graph/' + rename + '_' + tt + '_output.pdf')
-    if os.path.exists(tt + '_legend.pdf'):
-        os.rename(tt + '_legend.pdf', 'graph/' + rename + '_' + tt + '_legend.pdf')
-    shutil.copyfile('config/' + configpath + '.py', 'graph/' + configpath + '_' + rename + '.py')
+    if hasattr(p,'hsize' ): hsize=p.hsize
+    if hasattr(p,'vsize' ): vsize=p.vsize
+    if hasattr(p,'mksize'): mksize=p.mksize
+    if hasattr(p,'lnsize'): lnsize=p.lnsize
+    if hasattr(p, 'gl'): gl = p.gl
 
-def plot(configpath=None,dataname=None,rename=None,fixparam=None, xline=None,yline=None):
+    sysparam, nump=g.get_sysparam(data,range(0,3))
+    data=d.make_graphdata(data,sysparam,nump,0,3,1,2)
 
-    dataname,rename,fixparam, xline, yline=get_config(configpath,dataname,rename,fixparam, xline, yline)
-    data, sysparam, nump = prepare_data(dataname,fixparam)
     plot_paraset()
+    graph_paraset()
+    fig = plt.figure(figsize=(hsize*xplotnum,vsize*yplotnum))
 
-    pltn = [0]
-    if lnum is not None and ap==1:
-        if lineplot==1: fig = plt.figure(figsize=(0.1*float(len(sysparam[xnum])),16))
-        else:           fig = plt.figure(figsize=(6,4))
-        pltt=  range(nump[lnum])# plot table
-        plt.gcf().text(0.04, 0.75, ytitle, rotation=90)
-        plt.gcf().text(0.47, 0.18, xtitle)
-    else:
-        pltt=  [0]
-        if lnum is not None and ap==0:
-            pltn = range(nump[lnum])
+    xdata=sysparam[0]
+    pltt=  range(nump[2])# plot table
 
     if type(xline) != list: xline=[xline]*len(pltt)
     if type(yline) != list: yline=[yline]*len(pltt)
 
-    for n in pltn:
-        if lnum is None or ap==0: pltt = [n]
-        for l in pltt:
-            if lnum is not None and ap==1:
-                if lineplot==1: plt.subplot(len(pltt), 1, l+1)
-                else:           plt.subplot(5, 3, l+1)
-            else:
-                fig = plt.figure(figsize=(hsize, vsize))
-                plt.subplot(111,xlabel=xtitle,ylabel=ytitle)
+    for l in pltt:
+        plt.subplot(xplotnum, yplotnum, l+1)
+        graph_paraset()
 
-            graph_paraset(xline, yline, l)
+        if xline is not None: plt.hlines([xline[l]], xlm[0], xlm[1], "black", linewidth=0.7)  # hlines
+        if yline is not None: plt.vlines([yline[l]], ylm[0], ylm[1], "black", linewidth=0.7)  # hlines
 
-            plt.subplots_adjust(wspace=0.8/8, hspace=0.8/3)
-            #plt.xticks(color="None")
-            #plt.yticks(color="None")
+        plt.subplots_adjust(wspace=wspace, hspace=hspace)
+        if title is not None: plt.title(str(sysparam[2][l]),x=0.5,y=0.9)
 
-            if lnum is not None and ap==1:
-                if tc==1:
-                    plt.title(str(sysparam[lnum][l]),color=c.get_colorcode(int(sysparam[lnum][l])))
-                else:
-                    #plt.title(str(sysparam[p.lnum][l]),x=0.5,y=0.9)
-                    print('skip title')
-            else:
-                lb=None
-                clg='black'
-
-            ydata = data[:,:,l]
-            for yn in range(0, data.shape[1]):
-                if znum is not None and cl is None :
-                    lb = str(sysparam[znum][yn])
-                    if pc == 1:
-                        if lnum is None:
-                            clg = c.get_colorcode(int(sysparam[znum][yn]))
-                        else:
-                            clg = c.get_colorcode(int(sysparam[lnum][l]))
-                    else:
-                        clg = c.get_colorcode(int(360/data.shape[1]*yn))
-                elif cl is not None:
-                    lb=str(sysparam[znum][yn])
-                    if clg is not None:
-                        clg=c.get_colorcode(int(cl))
-                else:
-                    lb=None
-                    clg='black'
-
-                xdata=sysparam[xnum]
-
-                if znum is not None:
-                    if nump[znum] == 2 and yn == 1:
-                        plt.plot(xdata, ydata[:, yn], '-o', label=lb, color='black', linewidth=1, markersize=3)
-                        #plt.plot(xdata, ydata[:, yn], '--', label=lb, color='black',dashes=[2,1], linewidth=1, markersize=3)
-                        #plt.plot(xdata, ydata[:, yn], '-o', label=lb, color='red', linewidth=1, markersize=3)
-                    else:
-                        if dashline is not None:
-                            if yn==0: plt.plot(xdata, ydata[:, yn], '--', label=lb, color='black', linewidth=lnsize+1, markersize=mksize)
-                            else:     plt.plot(xdata, ydata[:, yn], gl, label=lb, color=clg, linewidth=lnsize, markersize=mksize)
-                        else:         plt.plot(xdata, ydata[:, yn], gl, label=lb, color=clg, linewidth=lnsize, markersize=mksize)
-                else:
-                    plt.plot(xdata, ydata[:, yn], gl, label=lb, color=clg, linewidth=lnsize, markersize=mksize, mec='k', mew=0.5)
-
-        if fitdata is not None:
-            fdata=np.loadtxt(fdata, delimiter=',')
-            plt.plot(fdata[:,0], fdata[:,1], ls='--', color='black',dashes=[2,1] , linewidth=1)
-
-        # save files
-        plt.close()
-        save(data, sysparam,configpath, rename, fig, lb, n)
-
-def plot2D(configpath=None,dataname=None,rename=None,fixparam=None, xline=None,yline=None):
-
-    dataname,rename,fixparam, xline, yline=get_config(configpath,dataname,rename,fixparam, xline, yline)
-    data, sysparam, nump = prepare_data(dataname,fixparam)
-    plot_paraset()
-
-    if type(xline) != list: xline=[xline]
-    if type(yline) != list: yline=[yline]
-    gd = d.save_graph_data('graph.csv', data, sysparam, xnum, znum, lnum)
-    fig = plt.figure(figsize=(hsize+0.3, hsize))
-    graph_paraset(xline, yline, 0)
-    plt.scatter(gd[:,0], gd[:,1], s=10, c=gd[:,2], cmap='seismic', vmin=-2.2, vmax=2.2)
-    plt.colorbar()
-
+        for yn in range(0, data.shape[1]):
+            plt.plot(xdata, data[:,yn,l], gl, label=str(sysparam[1][yn]), linewidth=lnsize, markersize=mksize)
 
     # save files
     plt.close()
-    save(data, sysparam,configpath, rename, fig, 'g', 0)
+    dt = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    fig.savefig('output_'+dt+'.pdf', transparent=True, bbox_inches='tight')
+    fig.legend().get_frame().set_alpha(1)
+    fig.savefig('legend_'+dt+'.pdf', transparent=True, bbox_inches='tight')
+
+    return dt
 
 if __name__== "__main__":
-    parser = argparse.ArgumentParser(description='plot_data')
-    parser.add_argument('--path', '-p', default='', type=str, help='config')
-    parser.add_argument('--dimension', '-d', default=None, type=str, help='config')
-    parser.set_defaults(test=False)
-    args = parser.parse_args()
-    if args.dimension!=None: plot2D(args.path)
-    else: plot(args.path)
+    configpath='config_0'
+    datapath='20221005163545/x6_y14_z1_l5.csv'
+
+    dt = plot(np.loadtxt(datapath,delimiter=','),configpath)
+    os.rename('output_'+dt+'.pdf', os.path.dirname(datapath)+'/output_'+dt+'.pdf')
+    os.rename('legend_'+dt+'.pdf', os.path.dirname(datapath)+'/legend_'+dt+'.pdf')
